9d479c35dfdd7023ea3a30e7767ac1ec816f4fc5
Adrian Sutton <adrian@queenofafrica.co.za>
2016-11-13 22:36:32 +0200

    Add files via upload
    Prime Factors 
    If you arrangement the prime factors of the natural numbers and place them in columns (shown above) with the lowest vale on the left, and increasing in value as you move to the right, another pattern emerges that reveals the primes.
    Notice, the spacing of the first occurrence of each prime is double that in the preceding column. eg. for 2 it goes 2,4,8,16,.... For 3 it is 3,6,12,24.... For 5 it is 5,10,20,40.... For 7 it is 7,14,28,56..... etc
    
    Take the first column only
    Notice that for each prime their occurrence begins with the prime number itself and then the second occurrence is the p-1, there after the patterns deviate somewhat and become more complex as the prime values increase. The spacing of 5 and 7 are somewhat  simpler to follow.  Using the prime 5 the pattern is easy 4,2,4,2……
    Using the prime 7 it is the pattern 6,4,2,4,2,4,6,2 …. That is continually repeated. Unfortunately I did not have enough data to explore the patterns fully. However the limite

---
+++ New Prime Code 10.py
@@ -0,0 +1,53 @@
+n = 123
+
+
+primeList = [2,3,5,7]
+
+h = 7
+
+while (primeList[-1] < n):   
+    a = 42 + h
+    if a % 49 == 0:
+        if a % 343 != 0:
+            primeList.append(a//49)
+    pass
+    b = 28 + a
+    if b % 49 == 0:
+        if b % 343 != 0:
+            primeList.append(b//49)
+    pass
+    c = 14 + b
+    if c % 49 == 0:
+        if c % 343 != 0:
+            primeList.append(c//49)
+    pass
+    d = 28 + c
+    if d % 49 == 0:
+        if d % 343 != 0:
+            primeList.append(d//49)
+    pass
+    e = 14 + d
+    if e % 49 == 0:
+        if e % 343 != 0:
+            primeList.append(e//49)
+    pass
+    f = 28 + e
+    if f % 49 == 0:
+        if f % 343 != 0:
+            primeList.append(f//49)
+    pass
+    g = 42 + f
+    if g % 49 == 0:
+        if g % 343 != 0:
+            primeList.append(g//49)
+    pass
+    h = 14 + g
+    if h % 49 == 0:
+        if h % 343 != 0:
+            primeList.append(h//49)
+    pass 
+
+primeList.remove(1)    
+print(primeList)
+print(len(primeList))
+
