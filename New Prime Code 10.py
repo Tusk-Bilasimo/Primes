664dc07798c0984a2fb542a91fc9ed22f8644ddf
Adrian Sutton <adrian@queenofafrica.co.za>
2016-11-20 10:16:40 +0200

    Update New Prime Code 10.py

--- New Prime Code 10.py
+++ New Prime Code 10.py
@@ -10,42 +10,41 @@ while (primeList[-1] < n):
     if a % 49 == 0:
         if a % 343 != 0:
             primeList.append(a//49)
-    pass
+    
     b = 28 + a
     if b % 49 == 0:
         if b % 343 != 0:
             primeList.append(b//49)
-    pass
+    
     c = 14 + b
     if c % 49 == 0:
         if c % 343 != 0:
             primeList.append(c//49)
-    pass
+    
     d = 28 + c
     if d % 49 == 0:
         if d % 343 != 0:
             primeList.append(d//49)
-    pass
+    
     e = 14 + d
     if e % 49 == 0:
         if e % 343 != 0:
             primeList.append(e//49)
-    pass
+    
     f = 28 + e
     if f % 49 == 0:
         if f % 343 != 0:
             primeList.append(f//49)
-    pass
+    
     g = 42 + f
     if g % 49 == 0:
         if g % 343 != 0:
             primeList.append(g//49)
-    pass
+    
     h = 14 + g
     if h % 49 == 0:
         if h % 343 != 0:
             primeList.append(h//49)
-    pass 
 
 primeList.remove(1)    
 print(primeList)
